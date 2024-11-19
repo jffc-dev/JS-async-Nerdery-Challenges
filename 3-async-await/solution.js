9b1e9e824131fd25992e1247de6cd99b4ad680ab
Javier Flores <jffc.dev@gmail.com>
2024-11-19 11:17:40 -0500

    feat(3-async/await): challenge solved using all and allSettled methods

--- 3-async-await/solution.js
+++ 3-async-await/solution.js
@@ -1,3 +1,5 @@
+const pricesFunction = require('./prices')
+const productsFunction = require('./products')
 /*
 INSTRUCTIONS
 
@@ -20,16 +22,90 @@ Example:
 9. as extra challenge: add Promise.race() and Promise.any(), and try to get the idea of what happens
 */
 
-function solution() {
-    // YOUR SOLUTION GOES HERE
-
-    // You generate your id value here
-
-    // You use Promise.all() here
-
-    // You use Promise.allSettled() here
-
-    // Log the results, or errors, here
+class Product {
+    constructor(id, name, price){
+        this.id = id
+        this.name = name
+        this.price = price
+    }
+    print(){
+        console.log(`ID: ${this.id} | product: ${this.name} | price: ${this.price.toFixed(2)} $`)
+    }
 }
 
-solution()
+class CustomError extends Error{
+    constructor(message, origin){
+        super(message)
+        this.origin = origin
+    }
+}
+
+const printError = (id, error) => {
+    console.error(`
+        === ERROR OCCURRED ===
+        - ID: ${id}
+        - Origin: ${error.origin || 'unknown'}
+        - Error Message: ${error.message}
+        =======================
+    `);
+}
+
+const generateId = async(delay) => {
+    if(delay){
+        await new Promise(resolve => setTimeout(resolve, delay))
+    }
+    const currentTime = Date.now()
+    return currentTime % 100
+}
+
+async function solution(allId, allSettledId) {
+    
+    console.log('Starting Promise.all proccess')
+    try {
+        const [name, price] = await Promise.all([
+            productsFunction(allId),
+            pricesFunction(allId)
+        ])
+        const product = new Product(allId, name, price)
+        product.print()
+
+    } catch (error) {
+        printError(allId, error)
+    }
+    console.log('Ending Promise.all proccess\n')
+
+    console.log('Starting Promise.allSettled proccess')
+
+    try {
+        const [nameResponse, priceResponse] = await Promise.allSettled([
+            productsFunction(allSettledId),
+            pricesFunction(allSettledId)
+        ])
+
+        if(nameResponse.status === 'rejected'){
+            throw new CustomError(nameResponse.reason.message, 'product')
+        }
+        if(priceResponse.status === 'rejected'){
+            throw new CustomError(priceResponse.reason.message, 'price')
+        }
+
+        const {value:name} = nameResponse
+        const {value:price} = priceResponse
+
+        const product = new Product(allSettledId, name, price)
+        product.print()
+    } catch (error) {
+        printError(allSettledId, error)
+    }
+    console.log('Ending Promise.allSettled proccess')
+} 
+
+
+(
+    async function(){
+        const generatedAllId = await generateId()
+        const generatedAllSettledId = await generateId(100)
+        solution(generatedAllId, generatedAllSettledId)
+    }
+)()
+
